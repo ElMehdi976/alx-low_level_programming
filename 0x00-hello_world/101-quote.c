f21759cd52a3e2d021e285df07ef26ea263c3a0b
El Mehdi <mehdi.benouakas@gmail.com>
2023-06-19 11:56:35 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: 1 to indicate failure
-*/
+ * main - Entry point
+ *
+ * Return: 1 to indicate failure
+ */
 int main(void)
 {
-	fwrite(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
-
 }
