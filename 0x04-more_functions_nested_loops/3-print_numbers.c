93b15a4424cf0d0d0d2a75cfb17a1328922a7ceb
El Mehdi <mehdi.benouakas@gmail.com>
2023-07-21 12:53:41 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,18 +3,16 @@
 /**
   *print_numbers - print 0 - 9
   *
-  *Return: always 0 
+  *Return: always 0
   */
 
 void print_numbers(void)
 {
 	int num = 0;
 
-	do
-	{
+	do {
 		_putchar(num + 48);
 		num++;
-	}
-	while (num >= 0 && num <= 9);
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
